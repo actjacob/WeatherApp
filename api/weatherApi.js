39799e0d4d6caca7ac49dee1e2eacc0a4a23241a
actjacob <yakupaktar99@gmail.com>
2024-10-23 21:13:49 +0300

    weatherApi key eklendi

--- api/weatherApi.js
+++ api/weatherApi.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 import { apikey } from '../constants/constans';
 
 const forecastEndpoint = (params) =>
-  'https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&${params.days}=1&aqi=no&alerts=no';
-const locationEndpoint = (params) =>
-  'https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&${params.days}=1&aqi=no&alerts=no';
+  `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&${params.days}=1&aqi=no&alerts=no`;
+const locationEndpoints = (params) =>
+  `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&${params.days}=1&aqi=no&alerts=no`;
